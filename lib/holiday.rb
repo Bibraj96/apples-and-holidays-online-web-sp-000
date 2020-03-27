f71c34062982c58a7a6a27452a1c71241827dbd2
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-27 16:48:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,11 +61,18 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  bbq = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |key, value|
+      if value == "BBQ"
+        bbq << key
+      end
+    end
+  end
 end
